82aa442a133fbdbc47984af9184622c01c2a69cf
karangorania <karangoraniya@gmail.com>
2022-06-20 22:24:48 +0530

    mumbai network config added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,17 @@ require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-etherscan');
 require('dotenv').config();
 
-const { RINKEBY_API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
+const { POLYGON_MUMBAI_API_URL, PRIVATE_KEY, POLYGON_API_KEY } = process.env;
 
 module.exports = {
   solidity: '0.8.4',
   networks: {
-    rinkeby: {
-      url: RINKEBY_API_URL,
+    mumbai: {
+      url: POLYGON_MUMBAI_API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_KEY,
+    apiKey: POLYGON_API_KEY,
   },
 };
